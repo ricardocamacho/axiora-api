644f54ae5c3c672ae598f586e1bcf6f7efebf1f8
Ricardo Camacho <ricardocamacho21@gmail.com>
2021-02-18 23:41:17 -0500

    Serverless http initial config

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+module.exports.handler = serverless(app);
