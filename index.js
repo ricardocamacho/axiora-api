9f28e17618c5c4d886fddd92aa1b6755de6bdbd2
Ricardo Camacho <ricardocamacho21@gmail.com>
2021-02-19 00:21:06 -0500

    Adding serverless offline support

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
+
+app.use(bodyParser.json({ strict: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
