b3f90da13448d2be97d669dad904c2aff4914b69
Ricardo Camacho <ricardocamacho21@gmail.com>
2021-02-20 01:56:08 -0500

    Mercadolibre inventory update for products with or without variations

--- src/update-inventory.js
+++ src/update-inventory.js
@@ -10,51 +10,67 @@ const updateInventoryMercadolibre = async (sku, quantity) => {
   // For each item ID
   let updatedItems = await Promise.all(
     itemIds.map(async itemId => {
-      // Get the item variations
-      const { variations } = await mercadolibreApi.getItem(itemId, {
-        attributes: 'variations',
+      let updatedItem;
+      // Get the item
+      const item = await mercadolibreApi.getItem(itemId, {
         include_attributes: 'all'
       });
-      // Iterate variations and update available quantity for selected SKU
-      variations.forEach(variation => {
-        delete variation.catalog_product_id;
-        const variationSkuAttr =
-          variation.attributes &&
-          variation.attributes.find(
-            attr => attr.id === 'SELLER_SKU' && attr.value_name === sku
-          );
-        if (variationSkuAttr) {
-          variation.available_quantity = quantity;
-        }
-      });
-      // Save the item variations
-      const updatedItem = await mercadolibreApi.updateItem(itemId, {
-        variations
-      });
+      // If the item has variations
+      if (item.variations && item.variations.length > 0) {
+        const { variations } = item;
+        // Iterate variations and update available quantity for selected SKU
+        variations.forEach(variation => {
+          delete variation.catalog_product_id;
+          const variationSkuAttr =
+            variation.attributes &&
+            variation.attributes.find(
+              attr => attr.id === 'SELLER_SKU' && attr.value_name === sku
+            );
+          if (variationSkuAttr) {
+            variation.available_quantity = quantity;
+          }
+        });
+        // Save the item variations
+        updatedItem = await mercadolibreApi.updateItem(itemId, {
+          variations
+        });
+      }
+      // If the item does not have variations
+      else {
+        updatedItem = await mercadolibreApi.updateItem(itemId, {
+          available_quantity: quantity
+        });
+      }
       return { id: itemId, ...updatedItem };
     })
   );
   // Format response for easier reading
-  updatedItems = updatedItems.map(item => ({
-    id: item.id,
-    variations: item.variations.map(variation => {
-      const formattedVariation = {
-        availableQuantity: variation.available_quantity
-      };
-      const variationSkuAttr =
-        variation.attributes &&
-        variation.attributes.find(
-          attr => attr.id === 'SELLER_SKU' && attr.value_name
-        );
-      if (variationSkuAttr) {
-        formattedVariation.sku = variationSkuAttr.value_name;
-      }
-      if (variationSkuAttr.value_name === sku) {
-        formattedVariation.updated = true;
-      }
-      return formattedVariation;
-    })
-  }));
+  updatedItems = updatedItems.map(item => {
+    const updatedItem = { id: item.id };
+    if (item.variations && item.variations.length) {
+      updatedItem.variations = item.variations.map(variation => {
+        const formattedVariation = {
+          availableQuantity: variation.available_quantity
+        };
+        const variationSkuAttr =
+          variation.attributes &&
+          variation.attributes.find(
+            attr => attr.id === 'SELLER_SKU' && attr.value_name
+          );
+        if (variationSkuAttr) {
+          formattedVariation.sku = variationSkuAttr.value_name;
+        }
+        if (variationSkuAttr.value_name === sku) {
+          formattedVariation.updated = true;
+        }
+        return formattedVariation;
+      });
+    } else {
+      updatedItem.availableQuantity = item.available_quantity;
+      updatedItem.updated = true;
+    }
+    return updatedItem;
+  });
   return updatedItems;
 };
 
