31f80409e6ceea26786cc7a2d7282f16befaf806
Ricardo Camacho <ricardocamacho21@gmail.com>
2021-04-04 01:24:05 -0500

    Removed mercadolibre api unused methods

--- src/api/mercadolibre.js
+++ src/api/mercadolibre.js
@@ -148,77 +148,10 @@ const getTokens = async (code, redirectUri) => {
   return response.data;
 };
 
-let userId = null;
-let refreshTokenValue = null;
-
-const setToken = token => {
-  if (token) {
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  } else {
-    delete axiosInstance.defaults.headers.common['Authorization'];
-  }
-};
-
-const setRefreshToken = token => {
-  refreshTokenValue = token;
-};
-
-const setUserId = id => {
-  userId = id;
-};
-
-const refreshToken = async () => {
-  const response = await axiosInstance.post('/oauth/token', {
-    grant_type: 'refresh_token',
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
-    refresh_token: refreshTokenValue
-  });
-  return response.data;
-};
-
-const getItemsBySKU = async (sku, aditionalParams) => {
-  const response = await axiosInstance.get(
-    `/users/${userId}/items/search?seller_sku=${sku}`,
-    {
-      params: aditionalParams
-    }
-  );
-  return response.data;
-};
-
-const getItem = async (itemId, aditionalParams) => {
-  const response = await axiosInstance.get(
-    `/items/${itemId}?attributes=variations`,
-    {
-      params: aditionalParams
-    }
-  );
-  return response.data;
-};
-
-const updateItem = async (itemId, item) => {
-  const response = await axiosInstance.put(`/items/${itemId}`, item);
-  return response.data;
-};
-
-const getOrder = async orderId => {
-  const response = await axiosInstance.get(`/orders/${orderId}`);
-  return response.data;
-};
-
 const mercadolibreApi = {
   stores,
   setStores,
-  getTokens,
-  setToken,
-  setRefreshToken,
-  setUserId,
-  refreshToken,
-  getItemsBySKU,
-  getItem,
-  updateItem,
-  getOrder
+  getTokens
 };
 
 module.exports = mercadolibreApi;
