5420b8ccbde013397e0e2862f498f98c51d9d45a
Ricardo Camacho <ricardocamacho21@gmail.com>
2025-03-13 02:27:40 -0500

    Using terraform aws modules instead of resources

--- server.tf
+++ server.tf
@@ -19,223 +19,172 @@ variable "aws_account_id" {
   default     = "571994632048"
 }
 
-variable "aws_default_vpc_id" {
-  description = "Default VPC ID"
-  type        = string
-  default     = "vpc-339e815b"
+module "axiora_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "axiora-vpc"
+  cidr = "10.0.0.0/22"
+
+  azs             = ["us-east-2a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.3.0/24"]
+
+  default_security_group_ingress = [
+    {
+      "cidr_blocks" : "0.0.0.0/0",
+      "ipv6_cidr_blocks" : "::/0",
+      "from_port" : 22,
+      "protocol" : "tcp",
+      "to_port" : 22
+    },
+    {
+      "cidr_blocks" : "0.0.0.0/0",
+      "ipv6_cidr_blocks" : "::/0",
+      "from_port" : 80,
+      "protocol" : "tcp",
+      "to_port" : 80
+    },
+    {
+      "cidr_blocks" : "0.0.0.0/0",
+      "ipv6_cidr_blocks" : "::/0",
+      "from_port" : 443,
+      "protocol" : "tcp",
+      "to_port" : 443
+    },
+  ]
+  default_security_group_egress = [
+    {
+      "cidr_blocks" : "0.0.0.0/0",
+      "ipv6_cidr_blocks" : "::/0",
+      "protocol" : "-1",
+    },
+  ]
 }
 
-resource "aws_security_group" "axiora_sg" {
-  name        = "axiora-sg"
-  description = "Allow SSH, HTTP, and HTTPS inbound traffic and all outbound traffic"
-  vpc_id      = var.aws_default_vpc_id
+module "iam_policy" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
 
-  tags = {
-    Name = "axiora-sg"
-  }
+  name        = "axiora-iam-policy"
+  description = "Axiora IAM policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:Query",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:dynamodb:us-east-2:${var.aws_account_id}:table/axiora-api-prod",
+        "arn:aws:dynamodb:us-east-2:${var.aws_account_id}:table/axiora-api-prod/index/SK-PK-index"
+      ]
+    },
+    {
+      "Action": [
+        "ses:SendEmail",
+        "ses:SendRawEmail"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:ses:us-east-2:${var.aws_account_id}:identity/hello@codingsquad.co"
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.axiora_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
+module "axiora_ec2_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+
+  role_name         = "axiora-ec2-role"
+  role_requires_mfa = false
+
+  create_role             = true
+  create_instance_profile = true
+
+  trusted_role_services = ["ec2.amazonaws.com"]
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    module.iam_policy.arn
+  ]
+  number_of_custom_role_policy_arns = 2
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
-  security_group_id = aws_security_group.axiora_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name                        = "axiora-ec2"
+  associate_public_ip_address = true
+
+  instance_type          = "t2.nano"
+  key_name               = "exploring-rsa-kp"
+  vpc_security_group_ids = [module.axiora_vpc.default_security_group_id]
+  subnet_id              = module.axiora_vpc.public_subnets[0]
+  iam_instance_profile   = module.axiora_ec2_role.iam_instance_profile_name
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv6" {
-  security_group_id = aws_security_group.axiora_sg.id
-  cidr_ipv6         = "::/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
+# resource "aws_lb" "axiora_lb" {
+#   name               = "axiora-lb"
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.axiora_sg.id]
+#   subnets            = [for subnet in data.aws_subnet.subnets : subnet.id]
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
-  security_group_id = aws_security_group.axiora_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
+# resource "aws_lb_target_group" "axiora_lb_tg" {
+#   name     = "axiora-lb-tg"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = var.aws_default_vpc_id
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv6" {
-  security_group_id = aws_security_group.axiora_sg.id
-  cidr_ipv6         = "::/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
+# resource "aws_lb_target_group_attachment" "axiora_lb_tg_attachments" {
+#   target_group_arn = aws_lb_target_group.axiora_lb_tg.arn
+#   target_id        = aws_instance.axiora_ec2.id
+#   port             = 80
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.axiora_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+# resource "aws_lb_listener" "axiora_lb_listener_http" {
+#   load_balancer_arn = aws_lb.axiora_lb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.axiora_sg.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.axiora_lb_tg.arn
+#   }
+# }
 
-resource "aws_iam_role" "axiora_ec2_role" {
-  name = "axiora-ec2-role"
+# resource "aws_lb_listener" "axiora_lb_listener_https" {
+#   load_balancer_arn = aws_lb.axiora_lb.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = "arn:aws:acm:us-east-2:${var.aws_account_id}:certificate/af05578f-dd55-4942-9a9d-e1843e18dd5f"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.axiora_lb_tg.arn
+#   }
+# }
 
-resource "aws_iam_instance_profile" "axiora_ec2_profile" {
-  name = "axiora-ec2-profile"
-  role = aws_iam_role.axiora_ec2_role.name
-}
+# data "aws_route53_zone" "axiora_aws_route53_zone" {
+#   name         = "axiora.co."
+#   private_zone = false
+# }
 
-resource "aws_iam_policy" "axiora_ec2_policy" {
-  name        = "axiora-ec2-policy"
-  description = "Allow Axiora EC2 to access Axiora resources"
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "dynamodb:Query",
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-        ]
-        Effect = "Allow"
-        Resource = [
-          "arn:aws:dynamodb:us-east-2:${var.aws_account_id}:table/axiora-api-prod",
-          "arn:aws:dynamodb:us-east-2:${var.aws_account_id}:table/axiora-api-prod/index/SK-PK-index",
-        ]
-      },
-      {
-        Action = [
-          "ses:SendEmail",
-          "ses:SendRawEmail",
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:ses:us-east-2:${var.aws_account_id}:identity/hello@codingsquad.co"
-      }
-    ]
-  })
-}
+# resource "aws_route53_record" "axiora_api_route53_record" {
+#   zone_id = data.aws_route53_zone.axiora_aws_route53_zone.zone_id
+#   name    = "api.axiora.co"
+#   type    = "A"
 
-resource "aws_iam_role_policy_attachment" "axiora_attach_policy1" {
-  role       = aws_iam_role.axiora_ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_role_policy_attachment" "axiora_attach_policy2" {
-  role       = aws_iam_role.axiora_ec2_role.name
-  policy_arn = aws_iam_policy.axiora_ec2_policy.arn
-}
-
-resource "aws_instance" "axiora_ec2" {
-  ami                  = "ami-0d0f28110d16ee7d6"
-  instance_type        = "t2.nano"
-  key_name             = "exploring-rsa-kp"
-  iam_instance_profile = aws_iam_instance_profile.axiora_ec2_profile.name
-
-  tags = {
-    Name = "axiora-ec2"
-  }
-
-  vpc_security_group_ids = [aws_security_group.axiora_sg.id]
-}
-
-data "aws_subnets" "default_vpc_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [var.aws_default_vpc_id]
-  }
-}
-
-data "aws_subnet" "subnets" {
-  for_each = toset(data.aws_subnets.default_vpc_subnets.ids)
-  id       = each.value
-}
-
-resource "aws_lb" "axiora_lb" {
-  name               = "axiora-lb"
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.axiora_sg.id]
-  subnets            = [for subnet in data.aws_subnet.subnets : subnet.id]
-}
-
-resource "aws_lb_target_group" "axiora_lb_tg" {
-  name     = "axiora-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.aws_default_vpc_id
-}
-
-resource "aws_lb_target_group_attachment" "axiora_lb_tg_attachments" {
-  target_group_arn = aws_lb_target_group.axiora_lb_tg.arn
-  target_id        = aws_instance.axiora_ec2.id
-  port             = 80
-}
-
-resource "aws_lb_listener" "axiora_lb_listener_http" {
-  load_balancer_arn = aws_lb.axiora_lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.axiora_lb_tg.arn
-  }
-}
-
-resource "aws_lb_listener" "axiora_lb_listener_https" {
-  load_balancer_arn = aws_lb.axiora_lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:acm:us-east-2:${var.aws_account_id}:certificate/af05578f-dd55-4942-9a9d-e1843e18dd5f"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.axiora_lb_tg.arn
-  }
-}
-
-data "aws_route53_zone" "axiora_aws_route53_zone" {
-  name         = "axiora.co."
-  private_zone = false
-}
-
-resource "aws_route53_record" "axiora_api_route53_record" {
-  zone_id = data.aws_route53_zone.axiora_aws_route53_zone.zone_id
-  name    = "api.axiora.co"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.axiora_lb.dns_name
-    zone_id                = aws_lb.axiora_lb.zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_lb.axiora_lb.dns_name
+#     zone_id                = aws_lb.axiora_lb.zone_id
+#     evaluate_target_health = false
+#   }
+# }
